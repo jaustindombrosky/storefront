7e3b4d5b3199a7bbf653a8d07e33c26533abe331
jaustindombrosky <austindombrosky@gmail.com>
2018-04-30 00:48:56 -0600

    vars

---
+++ sfcustomer.js
@@ -0,0 +1,6 @@
+var mysql = require("mysql");
+var request = require("request");
+var sub = require("sub");
+var table = require("table");
+var keys = require("./keys.js");
+var connection = mysql.createConnection(key.connection);

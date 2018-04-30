69b184dd6c1d1c3534eede05af2c0979fbb8e874
jaustindombrosky <austindombrosky@gmail.com>
2018-04-30 10:30:41 -0600

    selection and exit

--- sfcustomer.js
+++ sfcustomer.js
@@ -1,6 +1,25 @@
 var mysql = require("mysql");
 var request = require("request");
 var sub = require("sub");
-var table = require("table");
+var bTable = require("table");
 var keys = require("./keys.js");
 var connection = mysql.createConnection(key.connection);
+
+connection.connect(function(err){
+    if (err) throw err;
+});
+
+function selection(){
+    connection.query("SELECT * FROM products", function(err, res){
+       if (err) throw err;
+            var table = new bTable({
+                head: [""]
+            });
+    })
+}
+
+function exit(){
+    connection.end();
+    console.log("Thank you for your business!");
+}
+selection();
